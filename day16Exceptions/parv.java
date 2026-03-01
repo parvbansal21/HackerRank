db3133576bff4193fdcba4f41183fda1f7cd9f87
Parv Bansal <parv.bansal2024@vitstudent.ac.in>
2026-03-01 22:29:35 +0530

    yo

---
+++ day16Exceptions/parv.java
@@ -0,0 +1,20 @@
+package day16Exceptions;
+
+import java.io.*;
+
+
+
+public class parv {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = bufferedReader.readLine();
+        try{
+            int m=Integer.parseInt(S);
+            System.out.println(m);
+        }catch(Exception e){
+            System.out.println("Bad String");
+        }
+        bufferedReader.close();
+    }
+}
