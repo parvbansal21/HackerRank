47f72ccaff6f059a6df5e5ec272b1cf8befb0bbc
Parv Bansal <parv.bansal2024@vitstudent.ac.in>
2026-03-04 16:22:46 +0530

    exceptions

---
+++ day17Exceptionss/excep.java
@@ -0,0 +1,17 @@
+package day17Exceptionss;
+
+public class excep {
+    
+    public static void main(String[] args) {
+        try {
+            int a=10;
+            int b=0;
+            int c=a/b;
+            System.out.println(c);
+        } catch (Exception e) {
+            System.out.println("cannot divide by zero ");
+        }
+
+        System.out.println("program continues");
+    }
+}
