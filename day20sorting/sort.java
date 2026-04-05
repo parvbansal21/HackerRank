acfc725ea52f8324ca7e5bd22f960149c6910bdf
Parv Bansal <parv.bansal2024@vitstudent.ac.in>
2026-04-06 00:09:25 +0530

    sorting

--- day20sorting/sort.java
+++ day20sorting/sort.java
@@ -7,7 +7,7 @@ public class sort {
         Scanner sc =new Scanner(System.in);
         int n=sc.nextInt();
         int a[]=new int[n];
-        for (int i=0;i<=n;i++){
+        for (int i=0;i<n;i++){
              a[i]=sc.nextInt(); 
         }
         int noofswapping=0;
